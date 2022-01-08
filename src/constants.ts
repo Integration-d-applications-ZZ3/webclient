f797dd1cd469358e62f223b404c20635d7472647
slimf1 <slimane.f41@gmail.com>
2022-01-08 21:13:15 +0100

    Change back-end URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
 const constants = {
-  // API_BASE_URL: "https://back-end-integrationapplication.azurewebsites.net",
-  API_BASE_URL: "https://localhost:44396",
+  API_BASE_URL: "https://back-end-integrationapplication.azurewebsites.net",
   USER_LOCAL_STORAGE_KEY: "stock_user",
   COLOR_MODE_LOCALE_STORAGE_KEY: "stock_color_mode",
 };
